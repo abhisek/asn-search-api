964dba13e1f4eb194386fc242cc112e715a9c179
abhisek <abhisek.datta@gmail.com>
2019-12-09 21:10:01 +0530

    Initial import

---
+++ go.mod
@@ -0,0 +1,8 @@
+module .
+
+go 1.12
+
+require (
+	github.com/gorilla/mux v1.7.3 // indirect
+	github.com/sirupsen/logrus v1.4.2 // indirect
+)
---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	log "github.com/sirupsen/logrus"
+)
+
+const apiName = "asn-search-api"
+const apiVersion = "0.1.0"
+
+const listenHost = "0.0.0.0"
+const listenPort = 8000
+
+func initLogger() {
+	log.SetFormatter(&log.JSONFormatter{})
+	log.SetOutput(os.Stdout)
+	log.SetLevel(log.InfoLevel)
+}
+
+func queryAsnOrg(w http.ResponseWriter, r *http.Request) {
+	org := mux.Vars(r)["org"]
+	log.Infof("Querying ASN for organization: ", org)
+}
+
+func queryAsnDomain(w http.ResponseWriter, r *http.Request) {
+	domain := mux.Vars(r)["domain"]
+	log.Info("Querying ASN for domain: ", domain)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World!"))
+}
+
+func main() {
+	initLogger()
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", indexHandler)
+	r.HandleFunc("/domain/{domain}", queryAsnDomain)
+	r.HandleFunc("/org/{org}", queryAsnOrg)
+
+	log.Infof("Starting HTTP server on %s:%d", listenHost, listenPort)
+	http.ListenAndServe(fmt.Sprintf("%s:%d", listenHost, listenPort), r)
+}
